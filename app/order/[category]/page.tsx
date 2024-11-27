f54ce736575c85b3ed63f8040a5b521c48e6d8c1
Edson <EdsonP4100@gmail.com>
2024-11-27 15:15:57 -0800

    Category data updated

--- app/order/[category]/page.tsx
+++ app/order/[category]/page.tsx
@@ -1,6 +1,20 @@
+import ProductCard from '@/components/products/ProductCard';
+import { prisma } from '@/src/lib/Prisma';
+
 type CategoryProps = {
   category: string;
 };
+
+async function getProducts(category: string) {
+  const products = await prisma.product.findMany({
+    where: {
+      category: {
+        slug: category,
+      },
+    },
+  });
+  return products;
+}
 
 export default async function OrderPage({
   params,
@@ -8,6 +22,17 @@ export default async function OrderPage({
   params: Promise<CategoryProps>;
 }) {
   const { category } = await params;
-  console.log(category);
-  return <h1>OrderPage</h1>;
+  const products = await getProducts(category);
+  return (
+    <>
+      <h1 className="text-2xl my-10 ">
+        Elige y personaliza tú pedido a continuación
+      </h1>
+      <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-4">
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+    </>
+  );
 }
