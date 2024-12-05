07286d8455646239cf05ee56073c0a276b1dc95a
Edson <EdsonP4100@gmail.com>
2024-12-05 15:49:41 -0800

    Zod Schema add

--- components/order/OrderSummary.tsx
+++ components/order/OrderSummary.tsx
@@ -3,6 +3,8 @@ import { useStore } from '@/src/store';
 import ProductDetails from './ProductDetails';
 import { useMemo } from 'react';
 import { formatCurrency } from '@/src/utils';
+import { createOrder } from '@/actions/create-order-actions';
+import { OrderSchema } from '@/src/schema';
 
 export default function OrderSummary() {
   const order = useStore((state) => state.order);
@@ -10,12 +12,23 @@ export default function OrderSummary() {
     () => order.reduce((total, item) => total + item.quantity * item.price, 0),
     [order]
   );
+
+  const handleCreateOrder = (formData: FormData) => {
+    const data = {
+      name: formData.get('name'),
+    };
+    const result = OrderSchema.safeParse(data);
+    console.log(result);
+    return;
+    createOrder();
+  };
+
   return (
     <aside className="lg:h-screen lg:overflow-y-scroll md:w-64 lg:w-96 p-4">
       <h1 className="text-4xl text-center font-black">Mi Pedido</h1>
 
       {order.length === 0 ? (
-        <p className="mt-10 text-center">El carrito está vacío</p>
+        <p className="mt-10 text-center">El pedido está vacío</p>
       ) : (
         <div className="mt-5">
           {order.map((item) => (
@@ -26,6 +39,20 @@ export default function OrderSummary() {
             Total a pagar {''}
             <span className="font-bold">{formatCurrency(total)}</span>
           </p>
+
+          <form action={handleCreateOrder} className="w-full mt-10 space-y-5">
+            <input
+              type="text"
+              placeholder="Ingresa tú Nombre"
+              className="bg-white border border-gray-100 p-2 w-full"
+              name="name"
+            />
+            <input
+              type="submit"
+              className="py-2 rounded uppercase text-white font-bold bg-black w-full text-center cursor-pointer"
+              value="Confirmar Pedido"
+            />
+          </form>
         </div>
       )}
     </aside>
--- components/products/AddProductButton.tsx
+++ components/products/AddProductButton.tsx
@@ -11,7 +11,7 @@ export default function AddProductButton({ product }: AddProductButtonProps) {
   const addToOrder = useStore((state) => state.addToOrder);
   return (
     <button
-      className="bg-indigo-600 hover:bg-indigo-800 text-white w-full mt-5 p-3 font-bold cursor-pointer transition-colors"
+      className="bg-indigo-600 hover:bg-indigo-800 text-white uppercase w-full mt-5 p-3 font-bold cursor-pointer transition-colors"
       onClick={() => addToOrder(product)}
     >
       Agregar
