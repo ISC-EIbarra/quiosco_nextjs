fbab2de3aec2c29d331b1aa35506d55cf31372b7
Edson <EdsonP4100@gmail.com>
2025-02-12 19:19:55 -0800

    Image Uploader fix

--- components/products/ImageUpload.tsx
+++ components/products/ImageUpload.tsx
@@ -1,11 +1,21 @@
 'use client';
 import { CldUploadWidget } from 'next-cloudinary';
+import Image from 'next/image';
+import { useState } from 'react';
 import { TbPhotoPlus } from 'react-icons/tb';
 
 export default function ImageUpload() {
+  const [imageUrl, setImageUrl] = useState('');
+
   return (
     <CldUploadWidget
-      onSuccess={(result, { widget }) => {}}
+      onSuccess={(result, { widget }) => {
+        if (result.event === 'success') {
+          widget.close();
+          //@ts-ignore
+          setImageUrl(result.info?.secure_url);
+        }
+      }}
       uploadPreset="quioscoNext"
       options={{ maxFiles: 1 }}
     >
@@ -19,8 +29,20 @@ export default function ImageUpload() {
             >
               <TbPhotoPlus size={50} />
               <p className="text-lg font-semibold">Agregar Imagen</p>
+
+              {imageUrl && (
+                <div className="absolute inset-0 w-full h-full">
+                  <Image
+                    fill
+                    style={{ objectFit: 'contain' }}
+                    src={imageUrl}
+                    alt="Imagen de producto"
+                  />
+                </div>
+              )}
             </div>
           </div>
+          <input type="hidden" name="image" value={imageUrl} />
         </>
       )}
     </CldUploadWidget>
